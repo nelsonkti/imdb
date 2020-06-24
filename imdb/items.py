0f07860633a8d4a502a5b29d5e5ed302a6b322df
nelsonkti <nelsons.goole@gmail.com>
2020-06-24 15:11:39 +0800

    提交以前的项目

---
+++ imdb/items.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ImdbItem(Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    #url = scrapy.Field()    #url
+    #title = scrapy.Field()  #影片名
+
+    video_title = Field()
+    video_rating = Field()
+    video_name = Field()
+    video_alias = Field()
+    video_director = Field()
+    video_actor = Field()
+    video_length = Field()
+    video_language = Field()
+    video_year = Field()
+    video_type = Field()
+    video_color = Field()
+    video_area = Field()
+    video_voice = Field()
+    video_summary = Field()
+    video_url = Field()
